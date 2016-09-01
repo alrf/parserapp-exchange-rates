c1d0b8eae8ed93bbe3727a8a3218d4770141eabd
Alexander Fokin <alexander.fokin@genesys.com>
2016-09-01 23:38:14 +0400

    Init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/tmp/unicorn"
+shared_dir = "#{app_dir}/tmp"
 working_directory app_dir
 
 
@@ -10,11 +10,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/unicorn.stderr.log"
 stdout_path "#{shared_dir}/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/unicorn.pid"
+pid "#{shared_dir}/pids/unicorn.pid"
