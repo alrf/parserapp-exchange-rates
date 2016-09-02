5f57b9db7a687195fa62735083ea1f2af06d83c6
Alexander Fokin <alexander.fokin@genesys.com>
2016-09-02 19:35:37 +0400

    Parser

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,2 +1,39 @@
 class Parser < ApplicationRecord
+
+  require 'open-uri'
+  require 'nokogiri'
+
+  arr = {}
+  arr[0] = { 'lnk' => 'https://news.yandex.ru/quotes/2002.html', 'cmnt' => 'USD ллба', 'tbl' => 'usd_mmvb' }
+  arr[1] = { 'lnk' => 'https://news.yandex.ru/quotes/2000.html', 'cmnt' => 'EUR ллба', 'tbl' => 'eur_mmvb' }
+  arr[2] = { 'lnk' => 'https://news.yandex.ru/quotes/1.html', 'cmnt' => 'USD жа', 'tbl' => 'usd_cbr' }
+  arr[3] = { 'lnk' => 'https://news.yandex.ru/quotes/23.html', 'cmnt' => 'EUR жа', 'tbl' => 'eur_cbr' }
+  arr[4] = { 'lnk' => 'https://news.yandex.ru/quotes/10007.html', 'cmnt' => 'CHF жа', 'tbl' => 'chf_cbr' }
+
+  arr.each do |key, array|
+    puts "#{key}: #{array} #{array['lnk']}"
+    page = Nokogiri::HTML(open(array['lnk']))
+    #p page
+
+    date = []
+    page.css('.quote__date').each do |elem|
+      #p elem.content
+      date << elem.content
+    end
+    p date
+
+    value = []
+    page.css('.quote__value').each do |elem|
+      value << elem.content
+    end
+    p value
+
+    change = []
+    page.css('.quote__change').each do |elem|
+      change << elem.content
+    end
+    p change
+
+  end
+
 end
